4a8612509e2874ed41bc73f5e775abb59c0403a9
Sydney <manutsawee.louis@outlook.com>
2026-05-04 00:59:54 +0800

    rebuild: skinheaddress cmp

--- scripts/components/skinheaddress.lua
+++ scripts/components/skinheaddress.lua
@@ -1,60 +1,59 @@
-return Class(function(self, inst)
+local function OnSkinsChanged(inst)
+    local equip = inst.components.inventory:GetEquippedItem(EQUIPSLOTS.HEAD)
+    if equip == nil then
+        local build = inst.components.skinheaddress.headdresses[inst.AnimState:GetBuild()]
+        if build ~= nil then
+            inst.AnimState:Show("HAT")
+            inst.AnimState:OverrideSymbol("swap_hat", build, "swap_hat")
+        elseif not build then
+            inst.AnimState:Hide("HAT")
+            inst.AnimState:ClearOverrideBuild("swap_hat")
+        end
+    end
+end
+
+local function OnUnEquip(inst, data)
+    local eslot = data.eslot
+    if eslot ~= nil and eslot == EQUIPSLOTS.HEAD then
+        OnSkinsChanged(inst)
+    end
+end
+
+local function OnPlayerSpawned(world, data)
+    if data ~= nil and data.player ~= nil then
+        OnSkinsChanged(data.player)
+    end
+end
+
+local function OnRespawnFromGhost(inst)
+    inst:DoTaskInTime(3, OnSkinsChanged)
+end
+
+local function OnPlayerJoined(inst, player)
+    OnSkinsChanged(player)
+end
+
+local SkinHeaddress = Class(function(self, inst)
 
     assert(inst:HasTag("player"), "SkinHeaddress must use for player.")
 
     self.inst = inst
 
-    local _world = TheWorld
+    self.headdresses = {}
 
-    local headdresses = {}
-
-    function self:SetHeaddress(skin, headdress)
-        headdresses[skin] = headdress
-    end
-
-    function self:GetHeaddreass()
-        return headdresses
-    end
-
-    local function OnSkinsChanged(inst)
-        local equip = inst.components.inventory:GetEquippedItem(EQUIPSLOTS.HEAD)
-        if equip == nil then
-            local build = headdresses[inst.AnimState:GetBuild()]
-            if build ~= nil then
-                inst.AnimState:Show("HAT")
-                inst.AnimState:OverrideSymbol("swap_hat", build, "swap_hat")
-            elseif not build then
-                inst.AnimState:Hide("HAT")
-                inst.AnimState:ClearOverrideBuild("swap_hat")
-            end
-        end
-    end
-
-    local function OnUnEquip(inst, data)
-        local eslot = data.eslot
-        if eslot ~= nil and eslot == EQUIPSLOTS.HEAD then
-            OnSkinsChanged(inst)
-        end
-    end
-
-    local function OnPlayerSpawned(world, data)
-        if data ~= nil and data.player ~= nil then
-            OnSkinsChanged(data.player)
-        end
-    end
-
-    local function OnRespawnFromGhost(inst)
-        inst:DoTaskInTime(3, OnSkinsChanged)
-    end
-
-    local function OnPlayerJoined(inst, player)
-        OnSkinsChanged(player)
-    end
-
-    inst:ListenForEvent("onskinschanged", OnSkinsChanged)
-    inst:ListenForEvent("unequip", OnUnEquip)
-    inst:ListenForEvent("ms_respawnedfromghost", OnRespawnFromGhost)
-    inst:ListenForEvent("ms_newplayercharacterspawned", OnPlayerSpawned, _world)
-    inst:ListenForEvent("ms_playerjoined", OnPlayerJoined, _world)
-
+    self.inst:ListenForEvent("onskinschanged", OnSkinsChanged)
+    self.inst:ListenForEvent("unequip", OnUnEquip)
+    self.inst:ListenForEvent("ms_respawnedfromghost", OnRespawnFromGhost)
+    self.inst:ListenForEvent("ms_newplayercharacterspawned", OnPlayerSpawned, TheWorld)
+    self.inst:ListenForEvent("ms_playerjoined", OnPlayerJoined, TheWorld)
 end)
+
+function SkinHeaddress:SetHeaddress(skin, headdress)
+    self.headdresses[skin] = headdress
+end
+
+function SkinHeaddress:GetHeaddreass()
+    return self.headdresses
+end
+
+return SkinHeaddress
